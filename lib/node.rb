7f7dae8649af3bd9df3c01ce67403e10c8cf1499
Jane Kim <Janeekim12@gmail.com>
2018-03-19 14:42:01 -0600

    Finished iteration zero, starting on iteration one

---
+++ lib/node.rb
@@ -0,0 +1,9 @@
+class Node
+  attr_reader :data, :next_node
+
+  def initialize(data = "plop")
+    @data = data
+
+  end
+
+end
---
+++ test/node_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/node'
+require 'pry'
+
+class NodeTest < Minitest::Test
+
+  def test_node_exists
+    node = Node.new
+
+    assert_instance_of Node, node
+  end
+
+  def test_test_it_holds_data
+    node = Node.new("plop")
+
+    assert_equal "plop", node.data
+  end
+
+  def test_it_has_a_next_node
+    node = Node.new("plop")
+
+    assert_nil node.next_node
+  end
+
+end
